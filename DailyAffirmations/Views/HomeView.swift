9559c68a6b60d88a80b9371f39a77d3033309f63
MonicaVargasY <105678951+MonicaVargasY@users.noreply.github.com>
2025-08-05 16:31:49 -0600

    Created structure MVVM
    Created structure MVVM

---
+++ DailyAffirmations/Views/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  DailyAffirmations
+//
+//  Created by Monica Vargas on 2025-08-05.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Tu Afirmación del Día es:")
+            Text("Soy suficiente tal como soy.")
+        }
+        
+    }
+}
+
+#Preview {
+    HomeView()
+}
