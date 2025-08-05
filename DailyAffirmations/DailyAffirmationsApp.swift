b0b19809edbc01365940253fd7c5591a1abbec7c
MonicaVargasY <105678951+MonicaVargasY@users.noreply.github.com>
2025-08-05 16:03:49 -0600

    Initial Commit

---
+++ DailyAffirmations/DailyAffirmationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAffirmationsApp.swift
+//  DailyAffirmations
+//
+//  Created by Monica Vargas on 2025-08-05.
+//
+
+import SwiftUI
+
+@main
+struct DailyAffirmationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
